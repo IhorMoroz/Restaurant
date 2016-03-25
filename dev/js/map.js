d7b038633d1177fc4d41f8bad56ff5cb63177291
IhorMoroz <work.ihor.m@gmail.com>
2016-03-25 14:57:18 +0200

    add footer and main page

---
+++ dev/js/map.js
@@ -0,0 +1,23 @@
+var map,
+    marker,
+    position = {
+        lat: 48.312386,
+        lng: 31.526764
+    },
+    mapOption = {
+        center : position,
+        zoom : 10,
+        scrollwheel: false
+    },
+    markerOption = {
+        position: position,
+        map: map,
+        title: 'Good Food',
+        icon: "../img/app/marker3.png"
+    };
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), mapOption);
+    marker = new google.maps.Marker(markerOption);
+    marker.setMap(map);
+}
