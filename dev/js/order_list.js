d693bf82f17ce0d3689d5318979e410c6d2e1d4f
IhorMoroz <work.ihor.m@gmail.com>
2016-03-27 00:24:54 +0200

    user list first step

--- dev/js/order_list.js
+++ dev/js/order_list.js
@@ -1,27 +1,3 @@
-var map,
-    marker,
-    position = {
-        lat: 48.312386,
-        lng: 31.526764
-    },
-    mapOption = {
-        center : position,
-        zoom : 10,
-        scrollwheel: false
-    },
-    markerOption = {
-        position: position,
-        map: map,
-        title: 'Good Food',
-        icon: "../img/app/decoration/goodfood.png"
-    };
-
-function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), mapOption);
-    marker = new google.maps.Marker(markerOption);
-    marker.setMap(map);
-}
-
 $(document).ready(function(){
     var NO_VALID_VALUE = "This field must be a positive number greater than zero";
 
